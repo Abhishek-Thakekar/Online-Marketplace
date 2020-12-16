e7ee6e0cd4cb7305449fe1c893847ed35f9ae963
SohailBepari <sohailbepari10@gmail.com>
2020-12-16 14:31:06 +0530

    added Order and Product Model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -13,4 +13,43 @@
 //     isReceived
 // }
 
+const mongoose = require('mongoose');
 
+const OrderSchema = new mongoose.Schema({
+    productId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Product',
+        required : true
+    },
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    paymentType : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    isDelivered : {
+        type : Boolean,
+        default : false
+    },
+    isReceived : {
+        type : Boolean,
+        default : false
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+const Order = module.exports = mongoose.model('Order', OrderSchema);
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -7,3 +7,38 @@
 // availability
 // transactions => [transaction._id] 
 // date
+
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    productName : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    category : {
+        type : String,
+        required : true
+    },
+    sold : {
+        type : Number,
+        required : true
+    },
+    availability : {
+        type : Number,
+        required : true
+    },
+    transactions : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Order'
+    }],
+    date : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+const Product = module.exports = mongoose.model('Product', ProductSchema);
